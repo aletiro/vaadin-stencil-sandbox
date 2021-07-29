54511ce9f70ea23f86987bac9bd9b5a36b9c3467
Ale Tiro <Ale.Tiro@docuware.com>
2021-07-29 18:29:19 +0200

    fixed inline edit

--- vaadin-chart/src/components/my-component/my-component.tsx
+++ vaadin-chart/src/components/my-component/my-component.tsx
@@ -21,15 +21,8 @@ export class MyComponent {
         <vaadin-grid-column path="name.first" header="First Name"></vaadin-grid-column>
         <vaadin-grid-column path="name.last" header="Last name"></vaadin-grid-column>
          <vaadin-grid-pro-edit-column path="email" header="Email (edit)" width="150px">
-          <template>[[item.email]]</template>
-          <template class="editor">
-            <input type="email" value="{{item.email::input}}" />
-          </template>
         </vaadin-grid-pro-edit-column>
         <vaadin-grid-pro-edit-column path="location.zip" header="Zip code (edit)" text-align="end">
-          <template class="editor">
-            <input type="number" value="{{item.location.zip::input}}" />
-          </template>
         </vaadin-grid-pro-edit-column>
       </vaadin-grid-pro>
     </div>
